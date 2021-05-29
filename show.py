5a9c1446ccbb3d17b77f6bee57937abdae65362c
Masato <masato@example.com>
2021-05-29 13:17:01 +0900

    add today

--- show.py
+++ show.py
@@ -4,6 +4,6 @@
 import random
 
 with open("./hyakunin.txt", encoding="utf-8") as f:
-    wakas = [s.strip() for s in f.readlines()]
+	wakas = [s.strip() for s in f.readlines()]
 
 print("今日の一句" + wakas[random.randrange(len(wakas))])
